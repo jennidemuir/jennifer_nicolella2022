e4052f603199b12ffb478cd225c35eba44405b76
stellarwobble <jennidemuir@gmail.com>
2022-01-23 19:06:43 -0800

    changed BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/index";
 import ContactPage from './Pages/ContactPage.js'
 
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/contact" element={<ContactPage/>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
