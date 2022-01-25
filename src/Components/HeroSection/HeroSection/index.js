8a243eba122253d923195b4ff033e7cfea366f92
stellarwobble <jennidemuir@gmail.com>
2022-01-25 13:32:30 -0800

    added Hero section

---
+++ src/Components/HeroSection/HeroSection/index.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import Video from "../../../Media/horsevid1.mp4";
+import { Button } from "../../ButtonElements";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElements";
+
+const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+  return (
+    <HeroContainer id='home'>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Jennifer Nicolella</HeroH1>
+        <HeroP>
+         Animal Communicator
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to="services"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary="true"
+            dark="true"
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact={true}
+            offset={-80}
+          >
+            Get started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default HeroSection;
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Navbar from '../Components/Navbar'
 import Sidebar from '../Components/Sidebar'
+import HeroSection from "../Components/HeroSection/HeroSection";
 import InfoSection from '../Components/InfoSection'
 import Services from '../Components/Services'
 
@@ -10,6 +11,7 @@ import {
   homeObjThree,
   homeObjTwo,
 } from "../Components/InfoSection/Data";
+
 
 
 const Home = () => {
@@ -23,6 +25,7 @@ const Home = () => {
     <>
       <Navbar toggle={toggle} />
       <Sidebar toggle={toggle} isOpen={isOpen} />
+      <HeroSection/>
       <InfoSection {...homeObjOne} />
       <InfoSection {...homeObjTwo} />
       <InfoSection {...homeObjThree} />
